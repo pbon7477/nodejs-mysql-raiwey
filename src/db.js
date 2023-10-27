7861ac97c78c3aa1c726a5a0b8fb77bddcac0092
Pablo Bonvallet <pbon7477@gmail.com>
2023-10-27 19:30:51 -0400

    first commit

---
+++ src/db.js
@@ -0,0 +1,25 @@
+import {createPool} from 'mysql2/promise';
+import {DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,DB_PORT} from './config.js';
+
+
+export const pool = createPool({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
+})
+
+
+
+
+/*
+export const pool = createPool({
+    user: 'root',
+    password: '123456',
+    host: 'localhost',
+    port: 3306,
+    database: 'userdb'
+})
+
+*/
